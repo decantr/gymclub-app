832f70e968292c01c7b413e26a0597a644aa06a9
Shaw Eastwood <eastwoodshaw@gmail.com>
2022-01-15 15:18:02 +0000

    home: merge headers, add time closes

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -21,17 +21,17 @@ export class Tab2Page implements OnInit {
 			if (now.getHours() < 6 || now.getHours() > 20) {
 				this.openStatus = {icon: 'close-circle-outline', color: 'red', status: 'closed'}
 			} else if ( now.getHours() === 19) {
-				this.openStatus = {icon: 'alert-circle-outline', color: 'orange', status: 'closing'}
+				this.openStatus = {icon: 'alert-circle-outline', color: 'orange', status: 'closes at 8pm'}
 			} else {
-				this.openStatus = {icon: 'checkmark-circle-outline', color: 'green', status: 'open'}
+				this.openStatus = {icon: 'checkmark-circle-outline', color: 'green', status: 'open until 8pm'}
 			}
 		} else {
 			if (now.getHours() < 6 || now.getHours() > 22) {
 				this.openStatus = {icon: 'close-circle-outline', color: 'red', status: 'closed'}
 			} else if ( now.getHours() === 21) {
-				this.openStatus = {icon: 'alert-circle-outline', color: 'orange', status: 'closing'}
+				this.openStatus = {icon: 'alert-circle-outline', color: 'orange', status: 'closes at 10pm'}
 			} else {
-				this.openStatus = {icon: 'checkmark-circle-outline', color: 'green', status: 'open'}
+				this.openStatus = {icon: 'checkmark-circle-outline', color: 'green', status: 'open until 10pm'}
 			}
 		}
 	}
