8d94743c442298959a85a9ed4af9403664241b93
Shaw Eastwood <eastwoodshaw@gmail.com>
2022-01-17 23:54:05 +0000

    booking: add functional bookings

--- src/app/activities/bookings/bookings.component.ts
+++ src/app/activities/bookings/bookings.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { Booking } from 'src/app/models/booking';
 import { BookingService } from 'src/app/service/booking.service';
 import { UserService } from 'src/app/services/user.service';
@@ -12,18 +11,20 @@ import { formatRelative } from 'date-fns';
 })
 export class BookingsComponent implements OnInit {
 
-  public booking: Booking;
-  public now: string = new Date().toISOString().split('T')[0];
+  public bookings: Booking[] = [];
+  public newBooking: Booking;
+  public now: string;
   public out: string ='';
 
   constructor(
-    private router: Router,
     private userService: UserService,
     private bookingService: BookingService,
   ) { }
 
   ngOnInit() {
-    this.booking = {id: 1, user_id: this.userService.getId()};
+    this.now = this.bookingService.getOpen(new Date()).toISOString();
+
+    this.resetBooking();
   }
 
   public editBooking(event: Event, key: 'type'|'date_from'|'date_to', value?: string): void {
@@ -32,21 +33,33 @@ export class BookingsComponent implements OnInit {
     } else {
       if (key === 'date_from' || key === 'date_to' ) {
         if (value === 'next') {
-          const date = this.bookingService.getNext(this.booking)
-          this.booking.date_from = date;
+          const date = this.bookingService.getNext(this.newBooking)
+          this.newBooking.date_from = date;
           date.setHours(date.getHours() + 1);
-          this.booking.date_to = date;
+          this.newBooking.date_to = date;
         } else {
-          this.booking[key] = new Date(value);
+          this.newBooking[key] = new Date(value);
         }
       } else {
-        this.booking[key] = value;
+        this.newBooking[key] = value;
       }
     }
   }
 
+  public showBooking(booking: Booking) {
+    alert('book');
+  }
+
+  public finishBooking() {
+    this.bookings.push(this.newBooking);
+    this.newBooking = {id: 1, user_id: this.userService.getId()};
+    console.log(this.bookings);
+  }
+
   public resetBooking() {
-    this.booking = {id: 1, user_id: this.userService.getId()};
+    console.log(this.bookings);
+    this.newBooking = {id: 1, user_id: this.userService.getId()};
+    console.log(this.bookings);
   }
 
   public exportEvent() {
