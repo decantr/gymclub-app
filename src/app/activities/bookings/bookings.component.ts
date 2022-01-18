bfe819e2e41372d8855d6cd99f1e9d8d1c565dec
Shaw Eastwood <eastwoodshaw@gmail.com>
2022-01-18 00:04:48 +0000

    booking: add create entries from the calendar

--- src/app/activities/bookings/bookings.component.ts
+++ src/app/activities/bookings/bookings.component.ts
@@ -28,17 +28,21 @@ export class BookingsComponent implements OnInit {
   }
 
   public editBooking(event: Event, key: 'type'|'date_from'|'date_to', value?: string): void {
+    console.log(value);
     if (event) {
       this.out = JSON.stringify(event);
     } else {
       if (key === 'date_from' || key === 'date_to' ) {
         if (value === 'next') {
-          const date = this.bookingService.getNext(this.newBooking)
+          const date = this.bookingService.getNext(this.newBooking);
           this.newBooking.date_from = date;
           date.setHours(date.getHours() + 1);
           this.newBooking.date_to = date;
         } else {
-          this.newBooking[key] = new Date(value);
+          const date = new Date(value);
+          this.newBooking.date_from = new Date(value);
+          date.setHours(date.getHours() + 1);
+          this.newBooking.date_to = date;
         }
       } else {
         this.newBooking[key] = value;
@@ -53,13 +57,11 @@ export class BookingsComponent implements OnInit {
   public finishBooking() {
     this.bookings.push(this.newBooking);
     this.newBooking = {id: 1, user_id: this.userService.getId()};
-    console.log(this.bookings);
+    this.now = this.bookingService.getOpen(new Date()).toISOString();
   }
 
   public resetBooking() {
-    console.log(this.bookings);
     this.newBooking = {id: 1, user_id: this.userService.getId()};
-    console.log(this.bookings);
   }
 
   public exportEvent() {
